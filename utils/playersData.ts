ebd1a948c0ce8c8732015a2f1d8a520e663f252e
Nikhil Rachawar <nikhil123rachawar@gmail.com>
2024-04-27 11:26:16 +0530

    Players json data added

---
+++ utils/playersData.ts
@@ -0,0 +1,582 @@
+export const players = [
+  {
+    name: "SR Tendulkar",
+    country: "IND",
+    age: 49,
+  },
+  {
+    name: "RT Ponting",
+    country: "AUS",
+    age: 52,
+  },
+  {
+    name: "JH Kallis",
+    country: "ICC/SA",
+    age: 47,
+  },
+  {
+    name: "R Dravid",
+    country: "ICC/IND",
+    age: 50,
+  },
+  {
+    name: "AN Cook",
+    country: "ENG",
+    age: 40,
+  },
+  {
+    name: "KC Sangakkara",
+    country: "SL",
+    age: 46,
+  },
+  {
+    name: "BC Lara",
+    country: "ICC/WI",
+    age: 53,
+  },
+  {
+    name: "S Chanderpaul",
+    country: "WI",
+    age: 47,
+  },
+  {
+    name: "DPMD Jayawardene",
+    country: "SL",
+    age: 45,
+  },
+  {
+    name: "JE Root",
+    country: "ENG",
+    age: 32,
+  },
+  {
+    name: "AR Border",
+    country: "AUS",
+    age: 68,
+  },
+  {
+    name: "SR Waugh",
+    country: "AUS",
+    age: 57,
+  },
+  {
+    name: "SM Gavaskar",
+    country: "IND",
+    age: 74,
+  },
+  {
+    name: "Younis Khan",
+    country: "PAK",
+    age: 49,
+  },
+  {
+    name: "SPD Smith",
+    country: "AUS",
+    age: 33,
+  },
+  {
+    name: "HM Amla",
+    country: "SA",
+    age: 40,
+  },
+  {
+    name: "GC Smith",
+    country: "ICC/SA",
+    age: "-",
+  },
+  {
+    name: "GA Gooch",
+    country: "ENG",
+    age: 69,
+  },
+  {
+    name: "V Kohli",
+    country: "IND",
+    age: 34,
+  },
+  {
+    name: "Javed Miandad",
+    country: "PAK",
+    age: 66,
+  },
+  {
+    name: "Inzamam-ul-Haq",
+    country: "ICC/PAK",
+    age: 52,
+  },
+  {
+    name: "DA Warner",
+    country: "AUS",
+    age: 36,
+  },
+  {
+    name: "VVS Laxman",
+    country: "IND",
+    age: 48,
+  },
+  {
+    name: "AB de Villiers",
+    country: "SA",
+    age: 39,
+  },
+  {
+    name: "KS Williamson",
+    country: "NZ",
+    age: 32,
+  },
+  {
+    name: "MJ Clarke",
+    country: "AUS",
+    age: 42,
+  },
+  {
+    name: "ML Hayden",
+    country: "AUS",
+    age: 52,
+  },
+  {
+    name: "V Sehwag",
+    country: "ICC/IND",
+    age: 45,
+  },
+  {
+    name: "IVA Richards",
+    country: "WI",
+    age: 70,
+  },
+  {
+    name: "AJ Stewart",
+    country: "ENG",
+    age: 54,
+  },
+  {
+    name: "DI Gower",
+    country: "ENG",
+    age: 65,
+  },
+  {
+    name: "KP Pietersen",
+    country: "ENG",
+    age: 43,
+  },
+  {
+    name: "G Boycott",
+    country: "ENG",
+    age: 80,
+  },
+  {
+    name: "GS Sobers",
+    country: "WI",
+    age: 88,
+  },
+  {
+    name: "ME Waugh",
+    country: "AUS",
+    age: 54,
+  },
+  {
+    name: "MA Atherton",
+    country: "ENG",
+    age: 55,
+  },
+  {
+    name: "IR Bell",
+    country: "ENG",
+    age: 38,
+  },
+  {
+    name: "JL Langer",
+    country: "AUS",
+    age: 52,
+  },
+  {
+    name: "LRPL Taylor",
+    country: "NZ",
+    age: "-",
+  },
+  {
+    name: "MC Cowdrey",
+    country: "ENG",
+    age: 88,
+  },
+  {
+    name: "AD Mathews",
+    country: "SL",
+    age: 35,
+  },
+  {
+    name: "CG Greenidge",
+    country: "WI",
+    age: 68,
+  },
+  {
+    name: "Mohammad Yousuf",
+    country: "PAK",
+    age: 45,
+  },
+  {
+    name: "MA Taylor",
+    country: "AUS",
+    age: 57,
+  },
+  {
+    name: "CH Lloyd",
+    country: "WI",
+    age: 77,
+  },
+  {
+    name: "DL Haynes",
+    country: "WI",
+    age: 58,
+  },
+  {
+    name: "DC Boon",
+    country: "AUS",
+    age: 64,
+  },
+  {
+    name: "G Kirsten",
+    country: "SA",
+    age: 53,
+  },
+  {
+    name: "WR Hammond",
+    country: "ENG",
+    age: "-",
+  },
+  {
+    name: "CH Gayle",
+    country: "WI",
+    age: 43,
+  },
+  {
+    name: "SC Ganguly",
+    country: "IND",
+    age: 52,
+  },
+  {
+    name: "CA Pujara",
+    country: "IND",
+    age: 35,
+  },
+  {
+    name: "SP Fleming",
+    country: "NZ",
+    age: 53,
+  },
+  {
+    name: "Azhar Ali",
+    country: "PAK",
+    age: 37,
+  },
+  {
+    name: "GS Chappell",
+    country: "AUS",
+    age: 76,
+  },
+  {
+    name: "AJ Strauss",
+    country: "ENG",
+    age: 46,
+  },
+  {
+    name: "DG Bradman",
+    country: "AUS",
+    age: "-",
+  },
+  {
+    name: "M Muralidaran",
+    country: "ICC/SL",
+    age: 52,
+  },
+  {
+    name: "SK Warne",
+    country: "AUS",
+    age: "-",
+  },
+  {
+    name: "JM Anderson",
+    country: "ENG",
+    age: 41,
+  },
+  {
+    name: "A Kumble",
+    country: "IND",
+    age: 52,
+  },
+  {
+    name: "SCJ Broad",
+    country: "ENG",
+    age: 37,
+  },
+  {
+    name: "GD McGrath",
+    country: "AUS",
+    age: 52,
+  },
+  {
+    name: "NM Lyon",
+    country: "AUS",
+    age: 35,
+  },
+  {
+    name: "CA Walsh",
+    country: "WI",
+    age: 61,
+  },
+  {
+    name: "R Ashwin",
+    country: "IND",
+    age: 36,
+  },
+  {
+    name: "DW Steyn",
+    country: "SA",
+    age: 39,
+  },
+  {
+    name: "N Kapil Dev",
+    country: "IND",
+    age: 64,
+  },
+  {
+    name: "HMRKB Herath",
+    country: "SL",
+    age: 44,
+  },
+  {
+    name: "Sir RJ Hadlee",
+    country: "NZ",
+    age: 73,
+  },
+  {
+    name: "SM Pollock",
+    country: "SA",
+    age: 53,
+  },
+  {
+    name: "Harbhajan Singh",
+    country: "IND",
+    age: 42,
+  },
+  {
+    name: "Wasim Akram",
+    country: "PAK",
+    age: 57,
+  },
+  {
+    name: "CEL Ambrose",
+    country: "WI",
+    age: 58,
+  },
+  {
+    name: "M Ntini",
+    country: "SA",
+    age: 44,
+  },
+  {
+    name: "IT Botham",
+    country: "ENG",
+    age: 67,
+  },
+  {
+    name: "TG Southee",
+    country: "NZ",
+    age: 34,
+  },
+  {
+    name: "MD Marshall",
+    country: "WI",
+    age: 61,
+  },
+  {
+    name: "Waqar Younis",
+    country: "PAK",
+    age: 54,
+  },
+  {
+    name: "Imran Khan",
+    country: "PAK",
+    age: 70,
+  },
+  {
+    name: "DL Vettori",
+    country: "ICC/NZ",
+    age: 44,
+  },
+  {
+    name: "MA Starc",
+    country: "AUS",
+    age: 33,
+  },
+  {
+    name: "DK Lillee",
+    country: "AUS",
+    age: 73,
+  },
+  {
+    name: "WPUJC Vaas",
+    country: "SL",
+    age: 48,
+  },
+  {
+    name: "AA Donald",
+    country: "SA",
+    age: 56,
+  },
+  {
+    name: "RGD Willis",
+    country: "ENG",
+    age: 68,
+  },
+  {
+    name: "TA Boult",
+    country: "NZ",
+    age: 33,
+  },
+  {
+    name: "MG Johnson",
+    country: "AUS",
+    age: 42,
+  },
+  {
+    name: "I Sharma",
+    country: "IND",
+    age: 33,
+  },
+  {
+    name: "Z Khan",
+    country: "IND",
+    age: 40,
+  },
+  {
+    name: "B Lee",
+    country: "AUS",
+    age: 47,
+  },
+  {
+    name: "M Morkel",
+    country: "SA",
+    age: 38,
+  },
+  {
+    name: "LR Gibbs",
+    country: "WI",
+    age: 44,
+  },
+  {
+    name: "FS Trueman",
+    country: "ENG",
+    age: "-",
+  },
+  {
+    name: "DL Underwood",
+    country: "ENG",
+    age: "-",
+  },
+  {
+    name: "RA Jadeja",
+    country: "IND",
+    age: 34,
+  },
+  {
+    name: "JH Kallis",
+    country: "ICC/SA",
+    age: 47,
+  },
+  {
+    name: "K Rabada",
+    country: "SA",
+    age: 27,
+  },
+  {
+    name: "CJ McDermott",
+    country: "AUS",
+    age: 27,
+  },
+  {
+    name: "JR Hazlewood",
+    country: "AUS",
+    age: 32,
+  },
+  {
+    name: "KAJ Roach",
+    country: "WI",
+    age: 33,
+  },
+  {
+    name: "PJ Cummins",
+    country: "AUS",
+    age: 30,
+  },
+  {
+    name: "BS Bedi",
+    country: "IND",
+    age: 76,
+  },
+  {
+    name: "Danish Kaneria",
+    country: "PAK",
+    age: 41,
+  },
+  {
+    name: "N Wagner",
+    country: "NZ",
+    age: 33,
+  },
+  {
+    name: "J Garner",
+    country: "WI",
+    age: 72,
+  },
+  {
+    name: "JN Gillespie",
+    country: "AUS",
+    age: 49,
+  },
+  {
+    name: "GP Swann",
+    country: "ENG",
+    age: 44,
+  },
+  {
+    name: "JB Statham",
+    country: "ENG",
+    age: "-",
+  },
+  {
+    name: "MA Holding",
+    country: "WI",
+    age: 67,
+  },
+  {
+    name: "R Benaud",
+    country: "AUS",
+    age: "-",
+  },
+  {
+    name: "MJ Hoggard",
+    country: "ENG",
+    age: 48,
+  },
+  {
+    name: "GD McKenzie",
+    country: "AUS",
+    age: "-",
+  },
+  {
+    name: "Yasir Shah",
+    country: "PAK",
+    age: 37,
+  },
+  {
+    name: "BS Chandrasekhar",
+    country: "IND",
+    age: 77,
+  },
+  {
+    name: "Shakib Al Hasan",
+    country: "BAN",
+    age: 35,
+  },
+];
